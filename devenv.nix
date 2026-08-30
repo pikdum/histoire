09cc7408099a1548efcfef79ecdd140b4464fa10
pikdum <me@pikdum.dev>
2026-08-29 20:28:02 -0500

    chore: add postgres

--- devenv.nix
+++ devenv.nix
@@ -7,10 +7,24 @@
 }:
 
 {
+  languages.nix.enable = true;
   languages.elixir = {
     enable = true;
     package = pkgs.beam29Packages.elixir_1_20;
   };
 
-  languages.nix.enable = true;
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_18;
+    listen_addresses = "127.0.0.1";
+    port = 5432;
+    initialScript = ''
+      CREATE ROLE postgres SUPERUSER LOGIN PASSWORD 'postgres';
+    '';
+    initialDatabases = [
+      { name = "anime_data_dev"; }
+      { name = "anime_data_test"; }
+    ];
+  };
+
 }
